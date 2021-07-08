0731d151f257fd3509af1417f279159e8f1c5163
zjh6252007 <51406959+zjh6252007@users.noreply.github.com>
2021-07-08 17:16:18 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -1,20 +1,84 @@
 import socket
+import tkinter
+from threading import Thread
+from _thread import *
+import datetime
+import tkinter as tk
+import time
 
-ClientSocket = socket.socket()
-host = '127.0.0.1'
-port = 1233
 
-print('Waiting for connection')
-try:
-    ClientSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
+data = "20"
+time = "20"
+ne = ""
 
-Response = ClientSocket.recv(1024)
-while True:
-    Input = input('Say Something: ')
-    ClientSocket.send(str.encode(Input))
-    Response = ClientSocket.recv(1024)
-    print(Response.decode('utf-8'))
+def server():
+    
+    ip_port = ('127.0.0.1', 8888)
+    sk = socket.socket()
+    sk.connect(ip_port)
+    sk.settimeout(5)
+    while True:
+        global data
+        global time
+        global ne
+        data = sk.recv(1024).decode()
+        sk.send(b"ack")
+        time = sk.recv(1024).decode()
+        sk.send(b"ack2")
+        ne = sk.recv(1024).decode()
+    sk.close()
 
-ClientSocket.close()
+def getmessage():
+    global data
+    global time
+    
+    number = int(data)
+    if (number >= 0):
+        mess = data + " second early"
+    else:
+        mess = data + " second late"
+    var.set(mess)
+    root.after(2000, getmessage)
+    
+def gettime():
+    global time
+    time_ = int(time)
+    if (time_ >= 0):
+        mess =   "+"+ time +"kph"
+    else:
+        mess ="-" + time +"kph"
+    var1.set(mess)
+    root.after(2000,gettime)
+
+def getnev():
+    global ne
+    var2.set(ne)
+    root.after(2000,getnev)
+    
+root =tk.Tk()
+
+root.title('mention')
+
+var=tk.StringVar()
+var1=tk.StringVar()
+var2=tk.StringVar()
+color = int(data)
+
+if(color >= 0):
+    w =tk.Label(root, text="Hello Python!",textvariable=var,fg='red',font=("微软雅黑",40))
+    v =tk.Label(root, text= "hello",textvariable = var1,fg ='red',font=("微软雅黑",40))
+    z =tk.Label(root,text="",textvariable = var2,fg='black',font=("微软雅黑",40))
+else:
+    w =tk.Label(root, text="Hello Python!",textvariable=var,fg='green',font=("微软雅黑",40))
+    v =tk.Label(root, text= "hello",textvariable = var1,fg ='green',font=("微软雅黑",40))
+    z =tk.Label(root,text="",textvariable = var2, fg='black',font=("微软雅黑",40))
+
+var.set(getmessage())
+var1.set(gettime())
+var2.set(getnev())
+w.pack()
+v.pack()
+z.pack()
+if __name__ == '__main__':
+    Thread(target = server).start()
+    root.mainloop()
--- server.py
+++ server.py
@@ -1,21 +1,38 @@
-import socket
+import socketserver
+import json
+import time
 
-ip_port = ('127.0.0.1', 9999)
-sk = socket.socket()
-sk.connect(ip_port)
-sk.settimeout(5)
-data = sk.recv(1024).decode()
-print('server:', data)
-while True:
-    inp = input('player:').strip()
-    if not inp:
-        continue
+CV_message={"carla0":{"advice speed": -10, "time difference": 3, "navigation": "go straight"},"carla1":{}, "carla2":{}}
+adspeed = str(CV_message['carla0']['advice speed'])
+td = str(CV_message['carla0']['time difference'])
+na = CV_message['carla0']['navigation']
 
-    sk.sendall(inp.encode())
+#adspeed2 = str(CV_message['carla1']['advice speed'])
+#td2 = str(CV_message['carla1']['time difference'])
+#na2 = CV_message['carla1']['navigation']
 
-    if inp == 'exit':
-        print("thank you！")
-        break
-    data = sk.recv(1024).decode()
-    print('server:', data)
-sk.close()
+#adspeed3 = str(CV_message['carla2']['advice speed'])
+#td3 = str(CV_message['carla2']['time difference'])
+#na3 = CV_message['carla2']['navigation']
+class MyServer(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        conn = self.request
+        address = self.client_address
+        while True:
+            print(address)
+            cpeed = adspeed
+            ctd = td
+            cna = na
+            time.sleep(1)
+            conn.sendall(cpeed.encode('utf-8'))
+            ack = conn.recv(1024)
+            print(ack)
+            conn.sendall(ctd.encode('utf-8'))
+            ack2 = conn.recv(1024)
+            print(ack2)
+            conn.sendall(cna.encode('utf-8'))
+if __name__ == '__main__':
+    server = socketserver.ThreadingTCPServer(('127.0.0.1', 8888), MyServer)
+    print("start")
+    server.serve_forever()
