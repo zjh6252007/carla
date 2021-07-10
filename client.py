cbceb4e9134f192bcbd0d1946bfd2229ac07ef7a
zjh6252007 <51406959+zjh6252007@users.noreply.github.com>
2021-07-10 19:28:43 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -10,10 +10,10 @@ import time
 data = "20"
 time = "30"
 ne = ""
-color = 1
+color = "red"
 def server():
     
-    ip_port = ('127.0.0.1', 8888)
+    ip_port = ('192.168.1.149', 8888)
     sk = socket.socket()
     sk.connect(ip_port)
     sk.settimeout(5)
@@ -23,6 +23,7 @@ def server():
         global ne
         
         data = sk.recv(1024).decode()
+        print(data)
         if data == 'None':
             data = "0"
         sk.send(b"ack")
@@ -39,21 +40,25 @@ def getmessage():
     global data
     global color
     number = int(data)
-    if (number >= 0):
+    if (number > 0):
         mess = "+" + data + "kph"
-        color = 1
+        colorChangetoRed()
+    if(number == 0):
+        mess = ''
     else:
-        mess = "-" + data + "kph"
-        color = -1
+        mess =  data + "kph"
+        colorChangetoGreen()
     var.set(mess)
-    root.after(1000, getmessage)
+    root.after(200, getmessage)
     
 def gettime():
     global time
     
     time_ = int(time)
-    if (time_ >= 0):
+    if (time_ > 0):
         mess =    time +" late"
+    if(time_ == 0):
+        mess= ''
     else:
         mess = time +" early"
     var1.set(mess)
@@ -63,30 +68,36 @@ def getnev():
     global ne
     var2.set(ne)
     root.after(1000,getnev)
-    
+
+def colorChangetoRed():
+    w.config(fg = 'red')
+    v.config(fg = 'red')
+    root.update()
+def colorChangetoGreen():
+    w.config(fg = 'green')
+    v.config(fg = 'green')
+    root.update()
 root =tk.Tk()
 
 root.title('mention')
+root.attributes('-topmost',1)
+root.resizable(0,0)
 
 var=tk.StringVar()
 var1=tk.StringVar()
 var2=tk.StringVar()
 
-if(color == 1):
-    w =tk.Label(root, text="Hello Python!",textvariable=var1,fg='red',font=("微软雅黑",40))
-    v =tk.Label(root, text= "hello",textvariable = var,fg ='red',font=("微软雅黑",40))
-    z =tk.Label(root,text="",textvariable = var2,fg='black',font=("微软雅黑",40))
-if(color == -1):
-    w =tk.Label(root, text="Hello Python!",textvariable=var1,fg='green',font=("微软雅黑",40))
-    v =tk.Label(root, text= "hello",textvariable = var,fg ='green',font=("微软雅黑",40))
-    z =tk.Label(root,text="",textvariable = var2, fg='black',font=("微软雅黑",40))
+z =tk.Label(root,text="",textvariable = var2,fg="black",font=("微软雅黑",40))
+w =tk.Label(root, text="Hello Python!",textvariable=var1,fg=color,font=("微软雅黑",40))
+v =tk.Label(root, text= "hello",textvariable = var,fg =color,font=("微软雅黑",40))
+
 
 var.set(getmessage())
 var1.set(gettime())
 var2.set(getnev())
+z.pack()
 w.pack()
 v.pack()
-z.pack()
 if __name__ == '__main__':
     Thread(target = server).start()
     root.mainloop()
